84139dc2397f3b89e769811263ee3d16fc147543
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2024-05-08 16:29:03 +0200

    Update zadanie1.c

--- zadanie1.c
+++ zadanie1.c
@@ -1,3 +1,10 @@
+/*
+ * File:   newmainXC16.c
+ * Author: local
+ *
+ * Created on 8 maja 2024, 15:51
+ */
+
 // CONFIG2
 #pragma config POSCMOD = HS             // Primary Oscillator Select (HS Oscillator mode selected)
 #pragma config OSCIOFNC = OFF           // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as CLKO (FOSC/2))
@@ -37,73 +44,75 @@ int main(void) {
         prevS7 = PORTDbits.RD7;
         currentS6 = PORTDbits.RD6;
         currentS7 = PORTDbits.RD7;
-    }
-    
-    //zmiana podprogramu o 1 do przodu
-    if(currentS6 - prevS6 == 1)
-    {
-        if(currentS6 == 1)
+        
+        //zmiana podprogramu o 1 do przodu
+        if(currentS6 - prevS6 == 1)
         {
-            licznik++;
+            if(currentS6 == 1)
+            {
+                licznik++;
+            }
+        }
+    
+        // zmiana podprogramu o 1 do tylu
+        if(currentS7 - prevS7 == 1)
+        {
+            if(currentS7 == 1)
+            {
+                licznik--;
+            }
+        }
+        
+        // podprogram nr 1 - licznik zliczajacy w gore 0-255
+        if(licznik == 0)
+        {
+            portValue = 0;
+            while(1) 
+            {
+                LATA = portValue;   
+                __delay32(5000000);
+                portValue++;
+            }
+        }
+
+        // podprogram nr 2 - licznik zliczajacy w dol 255-0
+        if(licznik == 1)
+        {
+            portValue = 255;
+            while(1) 
+            {
+                LATA = portValue;   
+                __delay32(5000000);
+                portValue--;
+            }
+        }
+
+        // podprogram nr 3 - licznik w kodzie Graya zliczajacy w gore 0-255
+        if(licznik == 2)
+        {
+            portValue = 0;
+            while(1)
+            {
+                LATA = GrayConvert(portValue);
+                __delay32(5000000);
+                portValue++;
+            }
+        }
+
+        // podprogram nr 4 - licznik w kodzie Graya zliczajacy w dol 255-0
+        if(licznik == 3)
+        {
+            portValue = 255;
+            while(1)
+            {
+                LATA = GrayConvert(portValue);
+                __delay32(5000000);
+                portValue--;
+            }
         }
     }
     
-    // zmiana podprogramu o 1 do tylu
-    if(currentS7 - prevS7 == 1)
-    {
-        if(currentS7 == 1)
-        {
-            licznik--;
-        }
-    }
-    
-    // podprogram nr 1 - licznik zliczajacy w gore 0-255
-    if(licznik == 0)
-    {
-        portValue = 0;
-        while(1) 
-        {
-            LATA = portValue;   
-            __delay32(1000000);
-            portValue++;
-        }
-    }
-    
-    // podprogram nr 2 - licznik zliczajacy w dol 255-0
-    if(licznik == 1)
-    {
-        portValue = 255;
-        while(1) 
-        {
-            LATA = portValue;   
-            __delay32(1000000);
-            portValue--;
-        }
-    }
-    
-    // podprogram nr 3 - licznik w kodzie Graya zliczajacy w gore 0-255
-    if(licznik == 2)
-    {
-        portValue = 0;
-        while(1)
-        {
-            LATA = GrayConvert(portValue);
-            __delay32(1000000);
-            portValue++;
-        }
-    }
-    
-    // podprogram nr 4 - licznik w kodzie Graya zliczajacy w dol 255-0
-    if(licznik == 3)
-    {
-        portValue = 255;
-        while(1)
-        {
-            LATA = GrayConvert(portValue);
-            __delay32(1000000);
-            portValue--;
-        }
-    }
+
 
    return 0;
 }
