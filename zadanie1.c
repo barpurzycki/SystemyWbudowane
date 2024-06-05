e817df2146020b27d8f424215d3b76bca28792a7
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2024-06-05 15:47:20 +0200

    Update zadanie1.c

--- zadanie1.c
+++ zadanie1.c
@@ -1,165 +1,145 @@
-/*
- * File:   newmainXC16.c
- * Author: local
- *
- * Created on 8 maja 2024, 15:51
- */
-
 // CONFIG2
-#pragma config POSCMOD = HS             // Primary Oscillator Select (HS Oscillator mode selected)
-#pragma config OSCIOFNC = OFF           // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as CLKO (FOSC/2))
-#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
-#pragma config FNOSC = PRIPLL           // Oscillator Select (Primary Oscillator with PLL module (HSPLL, ECPLL))
-#pragma config IESO = OFF               // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) disabled)
+#pragma config POSCMOD = HS
+#pragma config OSCIOFNC = OFF
+#pragma config FCKSM = CSDCMD
+#pragma config FNOSC = PRIPLL
+#pragma config IESO = OFF
 
 // CONFIG1
-#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler (1:32,768)
-#pragma config FWPSA = PR128            // WDT Prescaler (Prescaler ratio of 1:128)
-#pragma config WINDIS = ON              // Watchdog Timer Window (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
-#pragma config FWDTEN = OFF             // Watchdog Timer Enable (Watchdog Timer is disabled)
-#pragma config ICS = PGx2               // Comm Channel Select (Emulator/debugger uses EMUC2/EMUD2)
-#pragma config GWRP = OFF               // General Code Segment Write Protect (Writes to program memory are allowed)
-#pragma config GCP = OFF                // General Code Segment Code Protect (Code protection is disabled)
-#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is disabled)
+#pragma config WDTPS = PS32768
+#pragma config FWPSA = PR128
+#pragma config WINDIS = ON
+#pragma config FWDTEN = OFF
+#pragma config ICS = PGx2
+#pragma config GWRP = OFF
+#pragma config GCP = OFF
+#pragma config JTAGEN = OFF
 
 #include "xc.h"
 #include "libpic30.h"
 
-// Zadanie 1.
+#define FOSC 40000000UL  // 40 MHz
+#define FCY (FOSC / 2)   // 20 MHz
 
 int GrayConvert(unsigned char i) {
     return (i >> 1) ^ i;
 }
 
+void delay_ms(unsigned int ms) {
+    __delay32(ms * (FCY / 1000));
+}
+
+void checkButtons(int *licznik, char *prev6, char *prev7) {
+    char current6 = PORTDbits.RD6;
+    char current7 = PORTDbits.RD7;
+
+    if (current6 == 1 && *prev6 == 0) {
+        (*licznik)++;
+    }
+
+    if (current7 == 1 && *prev7 == 0) {
+        (*licznik)--;
+    }
+
+    *prev6 = current6;
+    *prev7 = current7;
+
+    if (*licznik < 0) {
+        *licznik = 7;
+    } else if (*licznik > 7) {
+        *licznik = 0;
+    }
+}
+
 int main(void) {
-    int licznik = 6;
+    int licznik = 0;
     unsigned char portValue = 0;
-    char currentS6 = 0, currentS7 = 1, prevS6 = 0, prevS7 = 1;
+    char prev6, prev7;
     TRISA = 0x0000;
     TRISD = 0xFFFF;
-    
-    // przypisanie przycisków
-    while(1){
-        prevS6 = PORTDbits.RD6;
-        prevS7 = PORTDbits.RD7;
-        currentS6 = PORTDbits.RD6;
-        currentS7 = PORTDbits.RD7;
-        
-        //zmiana podprogramu o 1 do przodu
-        if(currentS6 - prevS6 == 1)
-        {
-            if(currentS6 == 1)
-            {
-                licznik++;
-            }
-        }
-    
-        // zmiana podprogramu o 1 do tylu
-        if(currentS7 - prevS7 == 1)
-        {
-            if(currentS7 == 1)
-            {
-                licznik--;
-            }
-        }
-        
-        // podprogram nr 1 - licznik zliczajacy w gore 0-255
-        if(licznik == 0)
-        {
-            portValue = 0;
-            while(1) 
-            {
-                LATA = portValue;   
-                __delay32(5000000);
-                portValue++;
-            }
-        }
 
-        // podprogram nr 2 - licznik zliczajacy w dol 255-0
-        if(licznik == 1)
-        {
-            portValue = 255;
-            while(1) 
-            {
-                LATA = portValue;   
-                __delay32(5000000);
-                portValue--;
-            }
-        }
+    prev6 = PORTDbits.RD6;
+    prev7 = PORTDbits.RD7;
 
-        // podprogram nr 3 - licznik w kodzie Graya zliczajacy w gore 0-255
-        if(licznik == 2)
-        {
-            portValue = 0;
-            while(1)
-            {
-                LATA = GrayConvert(portValue);
-                __delay32(5000000);
-                portValue++;
-            }
-        }
+    while (1) {
+        checkButtons(&licznik, &prev6, &prev7);
 
-        // podprogram nr 4 - licznik w kodzie Graya zliczajacy w dol 255-0
-        if(licznik == 3)
-        {
-            portValue = 255;
-            while(1)
-            {
-                LATA = GrayConvert(portValue);
-                __delay32(5000000);
-                portValue--;
-            }
-        }
-        
-        // podprogram nr 5 - 2x4 bitowy licznik w kodzie BCD zliczajacy w górę 0-99
-        if(licznik == 4)
-        {
-            LATA = (((portValue/10) << 4) | (portValue % 10));
-            __delay32(5000000);
-            if(portValue <= 99)
-            {
-                portValue++;
-            }
-            else 
-            {
+        switch (licznik) {
+            case 0: // Counter counting up 0-255
                 portValue = 0;
-            }
-        }
-        
-        // podprogram nr 6 - 2x4 bitowy licznik w kodzie BCD zliczajacy w dół 99-0
-        if(licznik == 5)
-        {
-            LATA = (((portValue/10) << 4) | (portValue % 10));
-            __delay32(5000000);
-            if(portValue > 0)
-            {
-                portValue--;
-            }
-            else
-            {
-                portValue = 99;
-            }  
-        }
-        
-        // 3 bitowy wężyk poruszający się lewo-prawo
-        if(licznik == 6)
-        {
-            unsigned char portValue[10] = {7, 14, 28, 56, 112, 224, 112, 56, 28, 14};
-            unsigned int i = 0;
-            while(1)
-            {
-                LATA = portValue[i];
-                __delay32(2000000);
-                i++;
-                if(i==10)
-                {
-                    i = 0;
+                while (licznik == 0) {
+                    LATA = portValue;
+                    delay_ms(200);
+                    portValue++;
+                    checkButtons(&licznik, &prev6, &prev7);
                 }
-            }
+                break;
+            case 1: // Counter counting down 255-0
+                portValue = 255;
+                while (licznik == 1) {
+                    LATA = portValue;
+                    delay_ms(200);
+                    portValue--;
+                    checkButtons(&licznik, &prev6, &prev7);
+                }
+                break;
+            case 2: // Gray code counter up 0-255
+                portValue = 0;
+                while (licznik == 2) {
+                    LATA = GrayConvert(portValue);
+                    delay_ms(200);
+                    portValue++;
+                    checkButtons(&licznik, &prev6, &prev7);
+                }
+                break;
+            case 3: // Gray code counter down 255-0
+                portValue = 255;
+                while (licznik == 3) {
+                    LATA = GrayConvert(portValue);
+                    delay_ms(200);
+                    portValue--;
+                    checkButtons(&licznik, &prev6, &prev7);
+                }
+                break;
+            case 4: // BCD counter up 0-99
+                portValue = 0;
+                while (licznik == 4) {
+                    LATA = ((portValue / 10) << 4) | (portValue % 10);
+                    delay_ms(200);
+                    portValue++;
+                    if (portValue > 99) portValue = 0;
+                    checkButtons(&licznik, &prev6, &prev7);
+                }
+                break;
+            case 5: // BCD counter down 99-0
+                portValue = 99;
+                while (licznik == 5) {
+                    LATA = ((portValue / 10) << 4) | (portValue % 10);
+                    delay_ms(200);
+                    portValue--;
+                    if (portValue > 99) portValue = 99;
+                    checkButtons(&licznik, &prev6, &prev7);
+                }
+                break;
+            case 6: // 3-bit snake moving left-right
+                {
+                    unsigned char snake[10] = {7, 14, 28, 56, 112, 224, 112, 56, 28, 14};
+                    unsigned int i = 0;
+                    while (licznik == 6) {
+                        LATA = snake[i];
+                        delay_ms(200);
+                        i++;
+                        if (i >= 10) i = 0;
+                        checkButtons(&licznik, &prev6, &prev7);
+                    }
+                }
+                break;
+            case 7: // queue
+                {
+                    
+                }
+                break;
         }
     }
-    
-
-
-   return 0;
+    return 0;
 }
