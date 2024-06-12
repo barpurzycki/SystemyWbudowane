96615c82a5b483fb3b47d48decaac48788c2005c
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2024-06-12 09:56:14 +0200

    Update zadanie5.c

--- zadanie5.c
+++ zadanie5.c
@@ -1,263 +1,187 @@
 /*
- * File:   newmainXC16.c
+ * File:   chess_clock.c
+ * Author: local
  *
- * Created on April 16, 2024, 4:50 PM
+ * Created on 29 maja 2024, 15:16
  */
 
-// CONFIG2
-#pragma config POSCMOD = NONE             // Primary Oscillator Select (HS Oscillator mode selected)
-#pragma config OSCIOFNC = OFF           // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as CLKO (FOSC/2))
-#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
-#pragma config FNOSC = FRC           // Oscillator Select (Primary Oscillator with PLL module (HSPLL, ECPLL))
-#pragma config IESO = OFF               // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) disabled)
-
-// CONFIG1
-#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler (1:32,768)
-#pragma config FWPSA = PR128            // WDT Prescaler (Prescaler ratio of 1:128)
-#pragma config WINDIS = ON              // Watchdog Timer Window (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
-#pragma config FWDTEN = OFF             // Watchdog Timer Enable (Watchdog Timer is disabled)
-#pragma config ICS = PGx2               // Comm Channel Select (Emulator/debugger uses EMUC2/EMUD2)
-#pragma config GWRP = OFF               // General Code Segment Write Protect (Writes to program memory are allowed)
-#pragma config GCP = OFF                // General Code Segment Code Protect (Code protection is disabled)
-#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is disabled)
+// CONFIGURATION BITS
+#pragma config POSCMOD = NONE             // Primary Oscillator Select (primary oscillator disabled)
+#pragma config OSCIOFNC = OFF             // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as CLKO (FOSC/2))
+#pragma config FCKSM = CSDCMD             // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = FRC                // Oscillator Select (Fast RC Oscillator without Postscaler)
+#pragma config IESO = OFF                 // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) disabled)
+#pragma config WDTPS = PS32768            // Watchdog Timer Postscaler (1:32,768)
+#pragma config FWPSA = PR128              // WDT Prescaler (1:128)
+#pragma config WINDIS = ON                // Watchdog Timer Window Mode disabled
+#pragma config FWDTEN = OFF               // Watchdog Timer disabled
+#pragma config ICS = PGx2                 // Emulator/debugger uses EMUC2/EMUD2
+#pragma config GWRP = OFF                 // Writes to program memory allowed
+#pragma config GCP = OFF                  // Code protection is disabled
+#pragma config JTAGEN = OFF               // JTAG port is disabled
 
 #include "xc.h"
-#include <string.h>
 #include <libpic30.h>
+#include <stdbool.h>
 
-#define FCY       4000000UL
-#define LCD_E     LATDbits.LATD4
-#define LCD_RW    LATDbits.LATD5
-#define LCD_RS    LATBbits.LATB15
-#define LCD_DATA  LATE
+// Defninicja makr tak by kod byl czytelny, przejrzysty, deskryptywny i przyjazny
+// uzytkownikowi
+#define FCY         4000000UL   // czestotliwosc robocza oscylatora jako polowa 
+                                //czestotliwosci (FNOSC = FRC -> FCY = 4000000)
+// Zdefiniowanie poszczegolnych pinow jako odpowiednie makra
+#define LCD_E       LATDbits.LATD4  
+#define LCD_RW      LATDbits.LATD5
+#define LCD_RS      LATBbits.LATB15
+#define LCD_DATA    LATE
 
-#define BTN_A (PORTDbits.RD6 == 0)
-#define BTN_B (PORTDbits.RD7 == 0)
-#define BTN_C (PORTAbits.RA7 == 0)
-#define BTN_D (PORTDbits.RD13 == 0)
+// Przypisanie wartosci poszcegolnych komend do wlasciwych makr
+#define LCD_CLEAR       0x01    //0b00000001
+#define LCD_HOME        0x02    //0b00000010
+#define LCD_ON          0x0C    //0b00001100
+#define LCD_OFF         0x08    //0b00001000
+#define LCD_CONFIG      0x38    //0b00111000
+#define LCD_CURSOR      0x80
+#define LINE1           0x00
+#define LINE2           0x40
+#define LCD_CUST_CHAR   0x40
+#define LCD_SHIFT_R     0x1D
+#define LCD_SHIFT_L     0x1B
 
-#define LCD_CLEAR               0b1
-#define LCD_CR                  0b10
-#define _LCD_INPUTMODE(ID, S)   (0b100 + (ID<<1) + S)
-#define _LCD_SCREENMODE(D,C,B)  (0b1000 + (D<<2) + (C<<1) + B)
-#define _LCD_CONFIG(DL,N,F)     (0b100000 + (DL<<4) + (N<<3) + (F<<2))
-#define LCD_ON                  _LCD_SCREENMODE(1,0,0)
-#define LCD_OFF                 _LCD_SCREENMODE(0,0,0)
-#define LCD_CONFIG              _LCD_CONFIG(1,1,0)
-#define LCD_CUST_CHAR           0x40
-#define LCD_SHIFT_R             0x1D
-#define LCD_CURSORPOS(P)        (0b10000000 + (P&0b1111111))
-//#define LCD_ON                  0x0C        //D = 1, C = 0
-//#define LCD_OFF                 0x08        //D = 0, C = 0
-//#define LCD_CONFIG              0x38        //DL = 1, N = 1, F = 0
+// Definicja funkcji delay w us i ms - operujacych na jednostkach czasu zamiast
+// cykli pracy oscylatora
 
-long synctime = 0;
-void sync_to(long t){
-    if (t > synctime){
-        __delay32(t-synctime);
-    }
-    synctime = 0;
-}
-void sleep_sync(long a){
-    __delay32(a);
-    synctime += a;
-}
-void sleep_sync_us(long us){
-    sleep_sync(FCY/1000000*us);
-}
-void sleep_sync_ms(long us){
-    sleep_sync(FCY/1000*us);
+void __delay_us(unsigned long us){
+    __delay32(us*FCY/1000000);
 }
 
-void toString(char* target, int digits, unsigned n){
-    for (int x = 0; x < digits; x++){
-        target[digits-1-x] = '0' + (n%10);
-        n /= 10;
-    }
+void __delay_ms(unsigned long ms){
+    __delay32(ms*FCY/1000);
 }
 
-void LCD_sendCommand(uint8_t command){
-    LCD_RW = 0;
-    LCD_RS = 0;
-    LCD_E = 1;
+void LCD_sendCommand(unsigned char command){
+    LCD_RW = 0;     // Zapis
+    LCD_RS = 0;     // Przesylanie komend
+    LCD_E = 1;      // Otwarcie transmisji danych
     LCD_DATA = command;
-    sleep_sync_us(50);
-    LCD_E = 0;
-    sleep_sync_us(50);
+    __delay_us(50); // Opoznienie konieczne dla zapisania danych.
+    LCD_E = 0;      // Konieczne zablokowanie transmisji po przeslaniu komunikatu.
 }
 
 void LCD_sendData(unsigned char data){
     LCD_RW = 0;
-    LCD_RS = 1;
+    LCD_RS = 1;     // Przesylanie danych
     LCD_E = 1;
     LCD_DATA = data;
-    sleep_sync_us(50);
+    __delay_us(50);
     LCD_E = 0;
-    sleep_sync_us(50);
 }
 
-void LCD_print(char* text, int line){
-    int c = 0;
-    if (line != 2){
-        LCD_sendCommand(LCD_CURSORPOS(line == 0 ? 0x00 : 0x40));
-    }
-    while (*text && c++ < 0x10){
-        LCD_sendData(*(text++));
+// Funkcja print wyswietlajaca kolejne 8-bitowe znaki w petli while - * oznacza
+// przypisanie nie wartosci zmiennej lecz jej adresu.
+
+void LCD_print(unsigned char* string){
+    while(*string){
+        LCD_sendData(*string++);
     }
 }
+
+void LCD_setCursor(unsigned char row, unsigned char col){
+    unsigned char address;
+    if(row == 1){
+        address = LCD_CURSOR + LINE1 + col;
+    }
+    if (row == 2){
+        address = LCD_CURSOR + LINE2 + col;
+    }
+    LCD_sendCommand(address);
+}
+
+// Funkcja inicjalizujaca wyswietlacz LCD. Wysyla niezbedne komendy jak LCD_CONFIG
+// i LCD_ON
 
 void LCD_init(){
-    sleep_sync_ms(20);
+    __delay_ms(20);
     LCD_sendCommand(LCD_CONFIG);
+    __delay_us(50);     // opoznienia wynikaja ze specyfikacji wyswietlacza i czasu
+                        // przetwarzania poszczegolnych komend
     LCD_sendCommand(LCD_ON);
+    __delay_us(50);
     LCD_sendCommand(LCD_CLEAR);
-    sleep_sync_ms(2);
+    __delay_ms(2);
 }
 
-
-struct PlayerTime {
-    unsigned min;
-    unsigned sec;
-};
-
-struct PlayerTime p1Time;
-struct PlayerTime p2Time;
-unsigned currentState = 0;
-unsigned lastPlayerWin = 0;
-
-void state_setTimes(){
-    char line1[0x10] = {' '};
-    char line2[0x10] = {' '};
-    memset(line1, ' ', 0x10);
-    memset(line2, ' ', 0x10);
-    line2[0] = '+';
-    line2[3] = '-';
-    line2[6] = '<';
-    line2[7] = '>';
-    line2[9] = 'S';
-    p1Time = (struct PlayerTime){20,0};
-    p2Time = (struct PlayerTime){20,0};
-    unsigned sel = 0;
-    while (1){
-        if (BTN_A){
-            unsigned* v = sel == 0 ? &p1Time.min : &p2Time.min;
-            if (*v < 99){
-                (*v)++;
-            }
-            sleep_sync_ms(250);
-        }
-        if (BTN_B){
-            unsigned* v = sel == 0 ? &p1Time.min : &p2Time.min;
-            if (*v > 1){
-                (*v)--;
-            }
-            sleep_sync_ms(250);
-        }
-        if (BTN_C){
-            sel = 1-sel;
-            sleep_sync_ms(250);
-        }
-        if (BTN_D){
-            currentState++;
-            sleep_sync_ms(250);
-            break;
-        }
-        
-        line1[0] = sel == 0 ? '>' : ' ';
-        toString(line1+1, 2, p1Time.min);
-        line1[3] = ':';
-        toString(line1+4, 2, p1Time.sec);
-        
-        line1[9] = sel == 1 ? '>' : ' ';
-        toString(line1+10, 2, p2Time.min);
-        line1[12] = ':';
-        toString(line1+13, 2, p2Time.sec);
-        
-        LCD_print(line1, 0);
-        LCD_print(line2, 1);
-        
-    }
+void displayTime(unsigned char row, unsigned int time) {
+    unsigned char minutes = time / 60;
+    unsigned char seconds = time % 60;
+    LCD_setCursor(row, 0);
+    LCD_sendData('0' + (minutes / 10)); 
+    LCD_sendData('0' + (minutes % 10)); 
+    LCD_sendData(':');
+    LCD_sendData('0' + (seconds / 10)); 
+    LCD_sendData('0' + (seconds % 10)); 
 }
-void state_ingame(){
-    char line1[0x10] = {' '};
-    char line2[0x10] = {' '};
-    memset(line1, ' ', 0x10);
-    memset(line2, ' ', 0x10);
-    while (1){
-        unsigned player = (ADC1BUF0 > 0b1000000000) ? 1 : 0;
-        struct PlayerTime* alt = (player ? &p2Time : &p1Time);
-        if (alt->sec-- == 0){
-            if (alt->min-- == 0){
-                lastPlayerWin = 1-player;
-                currentState++;
-                break;
-            }
-            alt->sec = 59;
-            
-        }
-        if (BTN_A){
-            currentState = 0;
-            break;
-            sleep_sync_ms(250);
-        }
-        
-        line1[0] = !player ? '|' : ' ';
-        toString(line1+1, 2, p1Time.min);
-        line1[3] = ':';
-        toString(line1+4, 2, p1Time.sec);
-        
-        line1[9] = player ? '|' : ' ';
-        toString(line1+10, 2, p2Time.min);
-        line1[12] = ':';
-        toString(line1+13, 2, p2Time.sec);
-        
-        LCD_print(line1, 0);
-        LCD_print(line2, 1);
-        
-        sync_to(FCY);
-    }
-}
-void state_end(){
-    char c[2] = {0};
-    c[0] = lastPlayerWin ? '2' : '1';
-    while (1){
-        LCD_print("WYGRYWA GRACZ ", 0);
-        LCD_print(c, 2);
-        LCD_print("                ", 1);
-        if (BTN_A){
-            currentState = 0;
-            break;
+
+bool readButton(volatile unsigned char *port, unsigned char pin) {
+    if ((*port & (1 << pin)) == 0) {
+        __delay_ms(50);
+        if ((*port & (1 << pin)) == 0) {
+            return true;
         }
     }
+    return false;
 }
 
 int main(void) {
-    
-    TRISA = 0b10000000;
-    TRISB = 0x7FFF; //potencjometr, czujnik temperatury
-    TRISD = 0b10000011000000;
-    TRISE = 0;
-    
-    AD1CON1 = 0x80E4;   //konfiguracja
-    AD1CON2 = 0x0404;   //kana?y, wielo czy jednokana?owo
-    AD1CON3 = 0x0F00;
-    AD1CHS = 0;         //wybór fizycznego urz?dzenia
-    AD1CSSL = 0x0020;
-    
-    LCD_init();
-    
-    p1Time = (struct PlayerTime){20,0};
-    p2Time = (struct PlayerTime){20,0};
-    
-    void (*states[])() = {
-        state_setTimes,
-        state_ingame,
-        state_end
-    };
-    
-    while(1){
-        states[currentState]();
+    bool currentP1 = 0, prevP1 = 0;
+    bool currentP2 = 0, prevP2 = 0;
+    unsigned int time1 = 300; // czas dla gracza 1 w sekundach
+    unsigned int time2 = 300; // czas dla gracza 2 w sekundach
+    bool player1_turn = true;
+
+    TRISB = 0x7FFF;   
+    TRISD = 0xFFE7;
+    TRISE = 0x0000;
+
+    LCD_init(); 
+
+    while(1) {
+        prevP1 = currentP1;
+        prevP2 = currentP2;
+
+        currentP1 = readButton(&PORTD, 6); // przycisk gracza 1
+        currentP2 = readButton(&PORTD, 7); // przycisk gracza 2
+
+        if (currentP1 && !prevP1 && !player1_turn) {
+            player1_turn = true;
+        }
+
+        if (currentP2 && !prevP2 && player1_turn) {
+            player1_turn = false;
+        }
+
+        if (player1_turn && time1 > 0) {
+            __delay_ms(1000);
+            time1--;
+        }
+
+        if (!player1_turn && time2 > 0) {
+            __delay_ms(1000);
+            time2--;
+        }
+
+        if (time1 == 0 || time2 == 0) {
+            LCD_setCursor(1, 0);
+            LCD_print("Time up!       ");
+            LCD_setCursor(2, 0);
+            if (time1 == 0) {
+                LCD_print("Player 2 wins!");
+            } else {
+                LCD_print("Player 1 wins!");
+            }
+            while(1); // Zatrzymanie gry
+        }
+
+        displayTime(1, time1);
+        displayTime(2, time2);
     }
-    
-    return 0;
 }
