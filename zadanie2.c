a12e23babd6e94fe2c00c4b201d5b89184ba5699
barpurzycki <92019696+barpurzycki@users.noreply.github.com>
2024-06-12 19:05:33 +0200

    Update zadanie2.c

--- zadanie2.c
+++ zadanie2.c
@@ -22,13 +22,16 @@
 #define FOSC 40000000UL  // 40 MHz
 #define FCY (FOSC / 2)   // 20 MHz
 
+// Funkcja opóźnienia w milisekundach
 void delay_ms(unsigned int ms) {
     __delay32(ms * (FCY / 1000));
 }
 
+// Funkcja sprawdzająca stan przycisku
 void checkButtons(unsigned int *licznik, char *prev6) {
     char current6 = PORTDbits.RD6;
 
+    // Jeśli przycisk został wciśnięty
     if (current6 == 1 && *prev6 == 0) {
         (*licznik)++;
     }
@@ -40,8 +43,10 @@ int main(void) {
     unsigned int licznik = 0, portValue = 0;
     char prev6 = 0;
     
+    // Ustawienie wszystkich pinów portu A jako wyjścia
     TRISA = 0x0000;
     
+    // Konfiguracja ADC 
     AD1CON1 = 0x80E4;
     AD1CON2 = 0x0404;
     AD1CON3 = 0x0F00;
@@ -49,35 +54,42 @@ int main(void) {
     AD1CSSL = 0x20;
 
     while (1) {
+        // Czekaj na zakończenie konwersji ADC
         while (!AD1CON1bits.DONE);
+        
+        // Sprawdzenie stanu przycisku
         checkButtons(&licznik, &prev6);
         
+        // Odczyt wartości z ADC i przesunięcie bitowe
         portValue = ADC1BUF0;
         portValue = portValue >> 2;
 
+        // Jeśli odczytana wartość jest większa lub równa 127
         if (portValue >= 127) { 
             licznik++;
-            delay_ms(1000);
+            delay_ms(1000); // Opóźnienie 1000 ms
 
+            // Jeśli licznik jest mniejszy niż 5
             if (licznik < 5) {
-                LATA = 1;
-                delay_ms(1000);
-                LATA = 0;
-                delay_ms(1000);
+                LATA = 1;  // Ustawienie stanu wysokiego na porcie A
+                delay_ms(1000); // Opóźnienie 1000 ms
+                LATA = 0;  // Ustawienie stanu niskiego na porcie A
+                delay_ms(1000); // Opóźnienie 1000 ms
             } else {
-                LATA = 255;
+                LATA = 255;  // Ustawienie wszystkich bitów portu A na wysoki stan
             }
 
+            // Czekaj, dopóki licznik jest większy lub równy 5 i przycisk jest wciśnięty
             while (licznik >= 5 && PORTDbits.RD6) {
-                LATA = 255;
-                checkButtons(&licznik, &prev6);
+                LATA = 255;  // Ustawienie wszystkich bitów portu A na wysoki stan
+                checkButtons(&licznik, &prev6);  // Sprawdzenie stanu przycisku
             }
         } else {
-            LATA = 0;
-            licznik = 0;
+            LATA = 0;  // Ustawienie stanu niskiego na porcie A
+            licznik = 0;  // Reset licznika
         }
 
-        delay_ms(100);
+        delay_ms(100);  // Opóźnienie 100 ms
     }
 
     return 0;
